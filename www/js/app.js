286f6c8e9e6aa00576e041ede45f45633f4cca15
Joseph Chingalo <profschingalo@gmail.com>
2016-04-08 12:19:39 +0300

    sql statements

--- www/js/app.js
+++ www/js/app.js
@@ -27,10 +27,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       var db = window.sqlitePlugin.openDatabase({name: "my.db"});
 
       db.transaction(function(tx) {
-        //tx.executeSql('DROP TABLE IF EXISTS test_table');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+        tx.executeSql('DROP TABLE IF EXISTS test_table');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data BLOB)');
 
-        tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+        tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", [{id:2,name:'joseph'}], function(tx, res) {
           alert("insertId: " + res.insertId );
         }, function(e) {
           alert.log("ERROR: " + e.message);
