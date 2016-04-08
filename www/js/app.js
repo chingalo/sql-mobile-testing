ea6764e69a871c06801e1231940a5dd1d7a6ba16
Joseph Chingalo <profschingalo@gmail.com>
2016-04-08 14:21:19 +0300

    sql statements

--- www/js/app.js
+++ www/js/app.js
@@ -28,7 +28,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       var data = {id:2,name:'joseph'};
       db.transaction(function(tx) {
         //tx.executeSql('DROP TABLE IF EXISTS test_table');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data LONGTEXT)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data BLOB)');
 
         tx.executeSql("INSERT INTO test_table (data) VALUES (?)", [data], function(tx, res) {
           alert("insertId: " + res.insertId );
