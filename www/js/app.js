1cbafe52a0313c0fe32a7174f8c1ca525133a7c2
Joseph Chingalo <profschingalo@gmail.com>
2016-04-09 21:09:48 +0300

    update records as well as checking empy name during adding new

--- www/js/app.js
+++ www/js/app.js
@@ -8,132 +8,140 @@
 var db = null;
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
 
 
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-     db = window.sqlitePlugin.openDatabase({name: "my.db"});
+      document.addEventListener("deviceready", onDeviceReady, false);
+      function onDeviceReady() {
+        db = window.sqlitePlugin.openDatabase({name: "my.db"});
 
-      db.transaction(function(tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS person (id integer primary key, data LONGTEXT)');
-      });
-    }
-  });
-})
-  .controller('mainCtr',function($scope,sqlLiteServices){
-    $scope.dataLoaded= [];
-    $scope.loadData = function(){
+        db.transaction(function (tx) {
+          tx.executeSql('CREATE TABLE IF NOT EXISTS person (id integer primary key, data LONGTEXT)');
+        });
+      }
+    });
+  })
+  .controller('mainCtr', function ($scope, sqlLiteServices) {
+    $scope.dataLoaded = [];
+    $scope.loadData = function () {
       var tableName = "person";
-      sqlLiteServices.getAllData(tableName).then(function(data){
+      sqlLiteServices.getAllData(tableName).then(function (data) {
         $scope.dataLoaded = data;
-      },function(error){
+      }, function (error) {
         alert('error : ' + JSON.stringify(error));
       });
     };
     $scope.data = {};
 
-    $scope.addData = function(){
+    $scope.addData = function () {
       var tableName = 'person';
-      var name = $scope.data.person;
       $scope.data = {};
-      var data = {id:$scope.dataLoaded.length + 1,name: name};
-      sqlLiteServices.insertData(tableName,data).then(function(data){
-
-      },function(error){
-        alert('error : ' + JSON.stringify(error));
-      });
+      if (angular.isDefined($scope.data.person)) {
+        var data = {id: $scope.dataLoaded.length + 1, name: $scope.data.person};
+        sqlLiteServices.insertData(tableName, data).then(function (data) {
+        }, function (error) {
+          alert('error : ' + JSON.stringify(error));
+        });
+      }
       $scope.loadData();
     };
     var tableName = "person";
-    $scope.delete = function(person){
-      sqlLiteServices.deleteData(tableName,person.id).then(function(){
+    $scope.delete = function (person) {
+      sqlLiteServices.deleteData(tableName, person.id).then(function () {
         alert('deleted success');
         $scope.loadData();
-      },function(error){
+      }, function (error) {
         alert('error : ' + JSON.stringify(error));
       });
     };
-    $scope.update = function(id){
+    $scope.update = function (person) {
       var tableName = "person";
-      sqlLiteServices.getData(tableName,(id + 1)).then(function(data){
-        alert('update data ' + JSON.stringify(data));
-      },function(error){
+      var UpdatedData = person;
+      UpdatedData.name = person.name + JSON.stringify(person.id);
+      sqlLiteServices.getData(tableName, (person.id)).then(function (data) {
+        alert('You select :' + JSON.stringify(data));
+        sqlLiteServices.updateData(tableName, person.id, UpdatedData).then(function () {
+          alert('Success update');
+          $scope.loadData();
+        }, function () {
+
+        });
+      }, function (error) {
         alert('error : ' + JSON.stringify(error));
       });
     };
 
 
   })
-.config(function($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
 
-  // Each tab has its own nav history stack:
+      // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+      })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+      .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
